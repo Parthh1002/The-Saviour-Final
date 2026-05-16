0077619d4b5af45a853db31c2d4c88c32706a8e2
aapnorasto <parthh1002@gmail.com>
2026-05-16 22:31:21 +0530

    fix: silence workspace root warning

--- the-saviour/next.config.ts
+++ the-saviour/next.config.ts
@@ -8,6 +8,9 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  experimental: {
+    outputFileTracingRoot: require('path').join(__dirname, '../'),
+  },
 };
 
 export default nextConfig;
